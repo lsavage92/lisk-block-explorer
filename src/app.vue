90d331a401051c706697bfd0eefdb705579d1523
Luke Savage <lsavage@digitalh2o.com>
2018-01-12 19:12:42 -0600

    added lisk logo to header

--- src/app.vue
+++ src/app.vue
@@ -1,45 +1,9 @@
-<script>
-  import navComponent from './components/nav.vue'
-  import logo from 'images/logo.png'
-
-  export default {
-    props: {
-      message: {
-        type: String,
-        default: 'You\'ve made it!'
-      }
-    },
-
-    data () {
-      return {
-        logo,
-      }
-    },
-
-    computed: {
-      contextMessage () {
-        return `${this.message} Now go build something!`
-      }
-    },
-
-    components: {
-      navComponent
-    }
-  }
-</script>
-
 <template>
   <div id="app">
     <div class="header">
-      <div class="logo">
-        <img :src="logo" />ue-build
-      </div>
-      <div class="tagline">
-        {{contextMessage}}
-      </div>
+      <img class="logo" src="./assets/images/lisk_logo.png" />
     </div>
     <div class="main">
-      <navComponent />
       <div class="content">
         <transition name="fade" mode="out-in" appear>
           <router-view></router-view>
